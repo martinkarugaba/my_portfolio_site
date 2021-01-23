51062acd12fa6a445f12d2a1b934fc2395031cf3
Martin Karugaba <martinkarugaba21@gmail.com>
2021-01-23 22:34:17 +0300

    changed the font to gugi

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('ul');
-const header = mobile_menu.querySelector('.header.container');
+const header = document.querySelector('.header.container');
 const menu_item = mobile_menu.querySelectorAll('li a');
+const dateContainer = document.getElementById('date');
 
+console.log(header);
+
+//* toggle navbar
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   mobile_menu.classList.toggle('active');
 });
-
-/***scroll effect */
+ 
+//* scroll effect
 document.addEventListener('scroll', () => {
   let scroll_position = window.scrollY;
 
@@ -25,3 +29,6 @@ menu_item.forEach((item) =>
     mobile_menu.classList.toggle('active');
   })
 );
+
+//* dynamic date
+dateContainer.innerHTML = newDate().getFullYear();
